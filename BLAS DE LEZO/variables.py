70fd43b405d4fe6208b8c6abbbeb9aed25b0f20d
PepeReinaCampo <125349576+PepeReinaCampo@users.noreply.github.com>
2024-03-08 18:36:34 +0100

    Add files via upload

---
+++ BLAS DE LEZO/variables.py
@@ -0,0 +1,18 @@
+# variables.py
+
+# Dimensiones del tablero
+TAMANO_TABLERO = 10
+
+# Barcos del juego y su eslora
+BARCOS = {
+    "Barco1": 1,
+    "Barco2": 1,
+    "Barco3": 1,
+    "Barco4": 1,
+    "Barco5": 2,
+    "Barco6": 2,
+    "Barco7": 2,
+    "Barco8": 3,
+    "Barco9": 3,
+    "Barco10": 4
+}
